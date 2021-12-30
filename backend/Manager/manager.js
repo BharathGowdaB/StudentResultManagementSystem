01cf42a9eadef38b14629903fab453ef5ea8ac35
Bharath Gowda B <bharathvishnubharath@gmail.com>
2021-12-30 15:19:43 +0530

    v1

---
+++ backend/Manager/manager.js
@@ -0,0 +1,13 @@
+const os_path = require('path');
+
+
+var path = {
+    root: 'C:/Users/BharathGowda B/Documents/GitHub/StudentResultManagementSystem',
+    src: './Src'
+
+} 
+
+
+module.exports = {
+    path
+}
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const  manager = require('./Manager/manager');
+
+const app = new express();
+const port = 8001;
+
+app.use(express.static(manager.path.src));
+
+app.get('/',function(req,res){
+   
+    res.sendFile(manager.path.src,{
+        maxAge:'1d',
+        root:manager.path.root
+    });
+})
+
+app.listen(port,'',() => {console.log('Server running at port :'+port)});
+
