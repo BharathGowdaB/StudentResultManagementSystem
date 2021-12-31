e9cd3d0a969c45eeb9db02ed6373cecc5f4cea93
Bharath Gowda B <bharathvishnubharath@gmail.com>
2021-12-31 19:43:41 +0530

    v1.06

---
+++ backend/_Routers/faculty/faculty.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+var faculty = express.Router()
+
+faculty.post('/login',async function(req,res){
+    
+})
+
+module.exports = faculty
---
+++ backend/_Routers/student/student.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+var student = express.Router()
+
+student.post('/Results',async function(req,res){
+    
+})
+
+module.exports = student
